63c2472abfee35d4873be6e67d8e651d881e7434
Semyon Metla <semenmetla6502@gmail.com>
2024-07-04 00:14:37 +0200

    thats it gg bois

--- screens/FinalScreen.js
+++ screens/FinalScreen.js
@@ -1,69 +1,105 @@
-import React from "react";
-import { View, Text, Image, TouchableOpacity, Share} from "react-native";
+import React, { useState } from "react";
+import { View, Text, Image, TouchableOpacity, Share } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { LinearGradient } from "expo-linear-gradient";
 import { styles } from "../styles/Styles";
 import * as MediaLibrary from "expo-media-library";
 
 const FinalScreen = ({ route }) => {
-  const { processedImage } = route.params;
+  const { processedImage, secondProcessedImage } = route.params;
   const navigation = useNavigation();
+
+  // State for managing current image
+  const [currentImage, setCurrentImage] = useState(processedImage);
 
   const handlePress = () => {
     navigation.navigate("Options");
   };
 
-  // Сохранение фото в галерею
+  // Toggle the current image
+  const toggleImage = () => {
+    setCurrentImage((prevImage) =>
+      prevImage === processedImage ? secondProcessedImage : processedImage
+    );
+  };
+
+  // Save the current image in device gallery
   const saveImageInDevice = async () => {
     try {
-      await MediaLibrary.saveToLibraryAsync(processedImage);
-      alert('Фото успешно сохранено в галерее');
-    }
-    catch (error) {
-      alert('Не удалось сохранить фото');
+      await MediaLibrary.saveToLibraryAsync(currentImage);
+      alert("Фото успешно сохранено в галерее");
+    } catch (error) {
+      alert("Не удалось сохранить фото");
     }
   };
 
-  // Поделиться фото
+  // Share the current image
   const shareImage = async () => {
     try {
       const shareOptions = {
-        message: 'Приложение "Прогноз Я" показало, как я буду выглядеть в будущем, если буду вести нездоровый образ жизни!',
-        url: processedImage,
+        message:
+          'Приложение "Прогноз Я" показало, как я буду выглядеть в будущем, если буду вести нездоровый образ жизни!',
+        url: currentImage,
       };
       await Share.share(shareOptions);
     } catch (error) {
-      alert('Произошла ошибка');
+      alert("Произошла ошибка");
     }
   };
 
   return (
-    <LinearGradient colors = {["#75BEEE", "#CCEDFF"]} style = {styles.container0}> 
-      {processedImage ? (
-      <View style = {[styles.container2, {width: "100%"}]}>
-          <View style = {styles.imageContainer2}>
-              <Image source = {{uri: processedImage}} style = {styles.image}/>
+    <LinearGradient colors={["#75BEEE", "#CCEDFF"]} style={styles.container0}>
+      {currentImage ? (
+        <View style={[styles.container2, { width: "100%" }]}>
+          <View style={{ alignItems: "center" }}>
+            <Text style={styles.mainText}>
+              {currentImage === processedImage
+                ? "Здоровый образ жизни"
+                : "Зависимый"}
+            </Text>
           </View>
-          <View style = {{alignItems: 'center'}}>
-            <Text style = {styles.header}>Сохранить и поделиться</Text>
-            <View style = {[styles.iconsContainer, {width: '35%'}]}>
-            <TouchableOpacity  style = {styles.iconButton} onPress = {saveImageInDevice}>
-                <LinearGradient colors = {["#1449B0", "#0E164B"]} style = {styles.iconBackground}>
-                  <Image source = {require('../assets/icons/Vector.png')}/>
+          <TouchableOpacity
+            style={styles.imageContainer2}
+            onPress={toggleImage}
+          >
+            <Image source={{ uri: currentImage }} style={styles.image} />
+          </TouchableOpacity>
+          <View style={{ alignItems: "center" }}>
+            <Text style={styles.header}>Сохранить и поделиться</Text>
+            <View style={[styles.iconsContainer, { width: "35%" }]}>
+              <TouchableOpacity
+                style={styles.iconButton}
+                onPress={saveImageInDevice}
+              >
+                <LinearGradient
+                  colors={["#1449B0", "#0E164B"]}
+                  style={styles.iconBackground}
+                >
+                  <Image source={require("../assets/icons/Vector.png")} />
                 </LinearGradient>
               </TouchableOpacity>
-              <TouchableOpacity  style = {styles.iconButton} onPress = {shareImage}>
-                <LinearGradient colors = {["#1449B0", "#0E164B"]} style = {styles.iconBackground}>
-                  <Image source = {require('../assets/icons/Arrow.png')}/>
+              <TouchableOpacity style={styles.iconButton} onPress={shareImage}>
+                <LinearGradient
+                  colors={["#1449B0", "#0E164B"]}
+                  style={styles.iconBackground}
+                >
+                  <Image source={require("../assets/icons/Arrow.png")} />
                 </LinearGradient>
               </TouchableOpacity>
             </View>
           </View>
-        </View>) : (<Text style = {[styles.errorText, {marginTop: 150}]}>Ошибка загрузки изображения</Text>)}
-      <TouchableOpacity style = {[styles.button, styles.shadow]} onPress = {handlePress}>
-          <Text style = {styles.buttonText}>Начать сначала</Text>
+        </View>
+      ) : (
+        <Text style={[styles.errorText, { marginTop: 150 }]}>
+          Ошибка загрузки изображения
+        </Text>
+      )}
+      <TouchableOpacity
+        style={[styles.button, styles.shadow]}
+        onPress={handlePress}
+      >
+        <Text style={styles.buttonText}>Начать сначала</Text>
       </TouchableOpacity>
-      
     </LinearGradient>
   );
 };
--- screens/LoadingPhotoScreen.js
+++ screens/LoadingPhotoScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useContext } from "react";
-import { View, Text} from "react-native";
+import { View, Text } from "react-native";
 import axios from "axios";
 import { useNavigation } from "@react-navigation/native";
 import * as FileSystem from "expo-file-system";
@@ -7,7 +7,7 @@ import { LinearGradient } from "expo-linear-gradient";
 import { GlobalContext } from "../context/GlobalContext";
 import { styles } from "../styles/Styles";
 
-REPLICATE_API_TOKEN = "r8_HSuX4OZtrjT7wCeSq2NOV0xOOEb7UlU1F9Hrm";
+const REPLICATE_API_TOKEN = "r8_Fgu32YmL9NugCpJlQTb7P0oo0RVndWs14CkUa";
 
 const LoadingPhotoScreen = ({ route }) => {
   const [currentDot, setCurrentDot] = useState(0);
@@ -17,19 +17,38 @@ const LoadingPhotoScreen = ({ route }) => {
   const navigation = useNavigation();
   const { userImage } = route.params;
 
-  const { yearValue, age, selectedSubstance, selectedDependency } =
-  useContext(GlobalContext);
+  const { yearValue, age } = useContext(GlobalContext);
+  const ageInput = (parseInt(age) + parseInt(yearValue)).toString();
 
-  const [ageInput, setAgeInput] = useState(parseInt(age) + parseInt(yearValue));
+  const { selectedSubstance, selectedDependency } = useContext(GlobalContext);
+
+  const setImageBasedOnSubstance = (selectedSubstance) => {
+    const substanceImages = {
+      alcohol:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Case_11-rightN.JPG/551px-Case_11-rightN.JPG?20100803114324",
+      cigarettes:
+        "https://ge.ceny.pro/wp-content/uploads/2021/12/nikomin-preimushchestva-1.jpg",
+      drugs: "https://piximus.net/media/20720/faces-of-meth-38.jpg",
+    };
+    return substanceImages[selectedSubstance];
+  };
+
+  const setStrengthBasedOnAddiction = (selectedDependency) => {
+    const substanceImages = {
+      moderate: 0.2,
+      severe: 0.85,
+    };
+    return substanceImages[selectedSubstance];
+  };
 
   useEffect(() => {
     const dotInterval = setInterval(() => {
       setCurrentDot((prevDot) => (prevDot + 1) % 3);
-    }, 500); // Обновляем анимацию каждые 0.5 секунды
+    }, 500);
 
     const timerInterval = setInterval(() => {
       setTimer((prevTimer) => prevTimer + 1);
-    }, 1000); // Обновляем таймер каждую секунду
+    }, 1000);
 
     return () => {
       clearInterval(dotInterval);
@@ -40,7 +59,7 @@ const LoadingPhotoScreen = ({ route }) => {
   useEffect(() => {
     const processImage = async () => {
       try {
-        const base64Image = await FileSystem.readAsStringAsync(userImage, {
+        const base64User = await FileSystem.readAsStringAsync(userImage, {
           encoding: FileSystem.EncodingType.Base64,
         });
 
@@ -50,7 +69,7 @@ const LoadingPhotoScreen = ({ route }) => {
             version:
               "9222a21c181b707209ef12b5e0d7e94c994b58f01c7b2fec075d2e892362f13c",
             input: {
-              image: `data:image/jpeg;base64,${base64Image}`,
+              image: `data:image/jpeg;base64,${base64User}`,
               target_age: ageInput,
             },
           },
@@ -89,19 +108,89 @@ const LoadingPhotoScreen = ({ route }) => {
           await new Promise((resolve) => setTimeout(resolve, 5000));
         }
 
-        if (predictionData.status === "succeeded" && !hasRedirected) {
-          setHasRedirected(true);
-          navigation.navigate("Final", {
-            processedImage: predictionData.output,
-          });
+        if (predictionData.status === "succeeded") {
+          const processedImageUrl = predictionData.output;
+
+          const response = await axios.post(
+            "https://api.replicate.com/v1/predictions",
+            {
+              version:
+                "db2c826b6a7215fd31695acb73b5b2c91a077f88a2a264c003745e62901e2867",
+              input: {
+                image_1: processedImageUrl,
+                image_2: setImageBasedOnSubstance(selectedSubstance),
+                control_image: processedImageUrl,
+                image_1_strength:
+                  setStrengthBasedOnAddiction(selectedDependency),
+                image_2_strength: 1,
+                prompt:
+                  "an svg illustration, sharp, solid color, thick outline",
+                upscale_2x: false,
+                animate: false,
+                render_method: "full",
+              },
+            },
+            {
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Token ${REPLICATE_API_TOKEN}`,
+              },
+            }
+          );
+
+          const secondPredictionId = response.data.id;
+
+          const getSecondPredictionStatus = async () => {
+            const statusResponse = await axios.get(
+              `https://api.replicate.com/v1/predictions/${secondPredictionId}`,
+              {
+                headers: {
+                  "Content-Type": "application/json",
+                  Authorization: `Token ${REPLICATE_API_TOKEN}`,
+                },
+              }
+            );
+            return statusResponse.data;
+          };
+
+          let secondPredictionData;
+          while (true) {
+            secondPredictionData = await getSecondPredictionStatus();
+            if (
+              secondPredictionData.status === "succeeded" ||
+              secondPredictionData.status === "failed"
+            ) {
+              break;
+            }
+            await new Promise((resolve) => setTimeout(resolve, 5000));
+          }
+
+          if (secondPredictionData.status === "succeeded" && !hasRedirected) {
+            console.log(processedImageUrl);
+            console.log(secondPredictionData.output[0]);
+            setHasRedirected(true);
+            navigation.navigate("Final", {
+              processedImage: processedImageUrl,
+              secondProcessedImage: secondPredictionData.output[0],
+            });
+          } else if (secondPredictionData.status === "failed") {
+            setError("Second prediction failed");
+          }
         } else if (predictionData.status === "failed") {
           setError("Prediction failed");
         }
       } catch (error) {
+        console.error(
+          "Error: ",
+          error.response ? error.response.data : error.message
+        );
         if (error.response && error.response.status === 401) {
           setError("Invalid API token");
         } else {
-          setError("An error occurred: " + error.message);
+          setError(
+            "An error occurred: " +
+              (error.response ? error.response.data.error : error.message)
+          );
         }
       }
     };
@@ -109,23 +198,24 @@ const LoadingPhotoScreen = ({ route }) => {
     if (!hasRedirected) {
       processImage();
     }
-  }, [hasRedirected, navigation]);
+  }, [hasRedirected, navigation, userImage, ageInput]);
 
   return (
-    <LinearGradient
-    colors = {["#1449B0", "#75BEEE"]} style = {styles.container1}>
-      <View style = {styles.dotsContainer}>
-        <View style = {[styles.dot, currentDot === 0 && styles.activeDot]} />
-        <View style = {[styles.dot, currentDot === 1 && styles.activeDot]} />
-        <View style = {[styles.dot, currentDot === 2 && styles.activeDot]} />
+    <LinearGradient colors={["#1449B0", "#75BEEE"]} style={styles.container1}>
+      <View style={styles.dotsContainer}>
+        <View style={[styles.dot, currentDot === 0 && styles.activeDot]} />
+        <View style={[styles.dot, currentDot === 1 && styles.activeDot]} />
+        <View style={[styles.dot, currentDot === 2 && styles.activeDot]} />
       </View>
       {error ? (
-        <Text style = {[styles.errorText, {width: "85%"} ]}>{error}</Text>
+        <Text style={[styles.errorText, { width: "85%" }]}>{error}</Text>
       ) : (
-        <View >
-          <Text style = {[styles.header, {fontSize: 24, color: 'white'}]}>Загружаем результат</Text>
-          <Text style = {styles.loadingText}>Прошло времени: {timer}</Text>
-          <Text style = {styles.loadingText}>Ожидаемый возраст: {ageInput}</Text>
+        <View>
+          <Text style={[styles.header, { fontSize: 24, color: "white" }]}>
+            Загружаем результат
+          </Text>
+          <Text style={styles.loadingText}>Прошло времени: {timer}</Text>
+          <Text style={styles.loadingText}>Ожидаемый возраст: {ageInput}</Text>
         </View>
       )}
     </LinearGradient>
